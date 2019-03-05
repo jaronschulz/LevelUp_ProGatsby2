65c91077e4c2d555738985ea9046063d89cc35d6
Jaron Schulz <jaronschulz@MacBook-Pro.local>
2019-03-05 11:05:33 +0100

    vid 3+4 importing images and basic gatsby formatting

---
+++ src/pages/about.js
@@ -0,0 +1,32 @@
+import React from 'react'
+
+import Layout from '../components/layout'
+
+const About = () => (
+  <Layout>
+    <div>
+      <h1>About Us</h1>
+      <p>
+        Hot chicken XOXO tbh, meh helvetica slow-carb next level keffiyeh la
+        croix keytar aesthetic. Seitan tbh swag pinterest. Street art migas blue
+        bottle, iceland bicycle rights yr whatever banjo poke unicorn
+        skateboard. Succulents crucifix waistcoat hoodie before they sold out,
+        gochujang four dollar toast semiotics 90's ennui shabby chic unicorn
+        activated charcoal fingerstache. Hexagon direct trade VHS tattooed
+        franzen crucifix deep v whatever kitsch. Actually VHS bicycle rights DIY
+        selfies shaman, hoodie occupy plaid bitters polaroid.
+      </p>
+      <p>
+        Polaroid cornhole franzen hammock jianbing shaman put a bird on it
+        chambray tumblr, poutine meh. Brooklyn roof party yuccie, banjo shaman
+        deep v copper mug twee af. Wayfarers pop-up vice fanny pack migas
+        chillwave hoodie ethical prism organic sartorial pug. Vinyl woke etsy
+        austin salvia mumblecore pickled whatever paleo 90's unicorn. Migas
+        butcher brunch heirloom, craft beer succulents taiyaki umami
+        single-origin coffee ramps lyft.
+      </p>
+    </div>
+  </Layout>
+)
+
+export default About
---
+++ src/pages/company/team.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+import Layout from '../../components/layout'
+
+const Team = () => (
+  <Layout>
+    <div>
+      <h1>The Team</h1>
+      <p>
+        Polaroid cornhole franzen hammock jianbing shaman put a bird on it
+        chambray tumblr, poutine meh. Brooklyn roof party yuccie, banjo shaman
+        deep v copper mug twee af. Wayfarers pop-up vice fanny pack migas
+        chillwave hoodie ethical prism organic sartorial pug. Vinyl woke etsy
+        austin salvia mumblecore pickled whatever paleo 90's unicorn. Migas
+        butcher brunch heirloom, craft beer succulents taiyaki umami
+        single-origin coffee ramps lyft.
+      </p>
+    </div>
+  </Layout>
+)
+
+export default Team
